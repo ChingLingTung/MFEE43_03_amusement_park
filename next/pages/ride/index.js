a98c3f3d49e1bdad9fccf56fdd0388343b1b5fce
ChingLingTung <littlesmall0540@gmail.com>
2024-01-02 10:46:28 +0800

    設施首頁大致上完成切版(細部待調整)

--- next/pages/ride/index.js
+++ next/pages/ride/index.js
@@ -16,7 +16,7 @@ export default function Ride() {
           <span className={styles.button} style={{width:95}}>輪椅可搭乘</span>
           <span type={'input'} className={styles.searchbar} placeholder={'請輸入設施名稱'}><IoSearchOutline style={{width:25}}/></span>
         </div>
-        <div className={styles.card_flex} style={{paddingLeft:250,paddingRight:250}}>
+        <div className={styles.card_flex}>
           <div>
             <Image className={styles.img} src={'/../../images/w1.jpg'} width={250} height={150} style={{marginLeft:10,marginRight:10}}/>
           </div>
@@ -41,7 +41,14 @@ export default function Ride() {
         </div>
       </div>
       <div className={styles.container}>
-        <div className={styles.theme_img}><Image className={styles.theme_img} src={'/../../images/w1.jpg'} resizeMode={'cover'} style={{width:'100%',height:200}} /></div>
+        <div className={styles.theme_img}>
+          {/* <Image className={styles.theme_img} src={'/../../images/w1.jpg'} resizeMode={'cover'} style={{width:'100%',height:200}} /> */}
+        </div>
+        <div className={styles.flex_center}>
+          <img src='/../../images/w1.jpg' width='380px' height='430px' className={styles.slide_card}/>
+          <img src='/../../images/w1.jpg' width='380px' height='430px' className={styles.slide_card}/>
+          <img src='/../../images/w1.jpg' width='380px' height='430px' className={styles.slide_card}/>
+        </div>
       </div>
         {/* https://stackoverflow.com/questions/45187785/react-native-how-to-make-image-width-100-percent-and-vertical-top */}
     </>
