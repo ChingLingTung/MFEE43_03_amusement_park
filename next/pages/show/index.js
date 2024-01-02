03e261e4dd8639f5139ce925b088bc067cbaa27f
ChingLingTung <littlesmall0540@gmail.com>
2024-01-02 23:13:37 +0800

    表演列表

--- next/pages/show/index.js
+++ next/pages/show/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/index.module.css'
+import styles from '@/styles/show.module.css'
 import { FaChevronRight } from "react-icons/fa6";
 
 const inter = Inter({ subsets: ['latin'] })
@@ -10,14 +10,14 @@ export default function Home() {
   return (
     <>
       <div className={styles.container}>
-        <h2 className={styles.title}>最新消息</h2>
+        <h2 className={styles.title}>表演節目</h2>
         <div className={styles.flex_center}>
           <div className={styles.card} style={{marginRight:10}}>
             <div>
               <Image className={styles.img} src={'/../images/newyear.jpg'} width={500} height={350}/>
             </div>
             <div className={styles.column} style={{width:500,height:250,padding:5}}>
-              <h3>標題</h3>
+              <h3>節目名稱</h3>
               <div className={styles.flex_spacebetween} style={{height:25}}>
                 <p>日期</p>
                 <button type='button' className={styles.button}>看更多<FaChevronRight style={{height:15,width:15}}/></button>
@@ -25,12 +25,13 @@ export default function Home() {
             </div>
           </div>
           <div className={styles.column} style={{width:630,height:600,marginLeft:10}}>
+            
             <div className={styles.mini_card}>
               <div>
                 <Image className={styles.mini_img} src={'/../images/newyear.jpg'} width={250} height={130}/>
               </div>
               <div style={{width:380,height:130}} className={styles.column}>
-                <h4 style={{paddingLeft:5}}>標題</h4>
+                <h4 style={{paddingLeft:5}}>節目名稱</h4>
                 <div className={styles.flex_spacebetween} style={{width:380,height:25,paddingLeft:5}}>
                   <p>日期</p>
                   <button type='button' className={styles.button}>看更多<FaChevronRight style={{height:15,width:15}}/></button>
@@ -42,7 +43,7 @@ export default function Home() {
                 <Image className={styles.mini_img} src={'/../images/newyear.jpg'} width={250} height={130}/>
               </div>
               <div style={{width:380,height:130}} className={styles.column}>
-                <h4 style={{paddingLeft:5}}>標題</h4>
+                <h4 style={{paddingLeft:5}}>節目名稱</h4>
                 <div className={styles.flex_spacebetween} style={{width:380,height:25,paddingLeft:5}}>
                   <p>日期</p>
                   <button type='button' className={styles.button}>看更多<FaChevronRight style={{height:15,width:15}}/></button>
@@ -54,7 +55,7 @@ export default function Home() {
                 <Image className={styles.mini_img} src={'/../images/newyear.jpg'} width={250} height={130}/>
               </div>
               <div style={{width:380,height:130}} className={styles.column}>
-                <h4 style={{paddingLeft:5}}>標題</h4>
+                <h4 style={{paddingLeft:5}}>節目名稱</h4>
                 <div className={styles.flex_spacebetween} style={{width:380,height:25,paddingLeft:5}}>
                   <p>日期</p>
                   <button type='button' className={styles.button}>看更多<FaChevronRight style={{height:15,width:15}}/></button>
@@ -66,7 +67,7 @@ export default function Home() {
                 <Image className={styles.mini_img} src={'/../images/newyear.jpg'} width={250} height={130}/>
               </div>
               <div style={{width:380,height:130}} className={styles.column}>
-                <h4 style={{paddingLeft:5}}>標題</h4>
+                <h4 style={{paddingLeft:5}}>節目名稱</h4>
                 <div className={styles.flex_spacebetween} style={{width:380,height:25,paddingLeft:5}}>
                   <p>日期</p>
                   <button type='button' className={styles.button}>看更多<FaChevronRight style={{height:15,width:15}}/></button>
@@ -76,50 +77,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className={styles.flex_spacebetween} style={{marginTop:200}}>
-        <div className={styles.flex_center} style={{width:800}}>
-          <div>
-            <h2 style={{textAlign:'center',lineHeight:5}}>水世界</h2>
-            <p>從適合一家大小的悠閒設施到刺激的設施一應俱全！</p>
-          </div>
-        </div>
-        <div>
-        <Image className={styles.theme_img} src={'/../images/w1.jpg'} width={1000} height={600}/>
-        </div>
-      </div>
-      <div className={styles.flex_spacebetween} style={{marginTop:200}}>
-        <div>
-        <Image className={styles.theme_img2} src={'/../images/w1.jpg'} width={1000} height={600}/>
-        </div>
-        <div className={styles.flex_center} style={{width:800}}>
-          <div>
-            <h2 style={{textAlign:'center',lineHeight:5}}>水世界</h2>
-            <p>從適合一家大小的悠閒設施到刺激的設施一應俱全！</p>
-          </div>
-        </div>
-      </div>
-      <div className={styles.flex_spacebetween} style={{marginTop:200}}>
-        <div className={styles.flex_center} style={{width:800}}>
-          <div>
-            <h2 style={{textAlign:'center',lineHeight:5}}>水世界</h2>
-            <p>從適合一家大小的悠閒設施到刺激的設施一應俱全！</p>
-          </div>
-        </div>
-        <div>
-        <Image className={styles.theme_img} src={'/../images/w1.jpg'} width={1000} height={600}/>
-        </div>
-      </div>
-      <div className={styles.flex_spacebetween} style={{marginTop:200}}>
-        <div>
-        <Image className={styles.theme_img2} src={'/../images/w1.jpg'} width={1000} height={600}/>
-        </div>
-        <div className={styles.flex_center} style={{width:800}}>
-          <div>
-            <h2 style={{textAlign:'center',lineHeight:5}}>水世界</h2>
-            <p>從適合一家大小的悠閒設施到刺激的設施一應俱全！</p>
-          </div>
-        </div>
-      </div>
+
     </>
   )
 }
