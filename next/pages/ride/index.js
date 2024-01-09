e3e27586bca540ba2a8e70db9c57e6fc4a154ade
ChingLingTung <littlesmall0540@gmail.com>
2024-01-09 17:27:39 +0800

    嘗試按鈕搜尋改變樣式和值(尚未完成)

--- next/pages/ride/index.js
+++ next/pages/ride/index.js
@@ -6,7 +6,6 @@ import Image from 'next/image';
 import { Layout } from '@/component/layout';
 import Head from 'next/head';
 import { RIDE_LIST } from '@/component/ride-const';
-import ThemeContext from '@/context/theme-context';
 import { useRouter } from 'next/router';
 import { useState,useContext,useEffect } from 'react';
 import Link from 'next/link';
@@ -14,20 +13,84 @@ import Link from 'next/link';
 export default function Ride() {
   const [data, setData] = useState({});
   const router = useRouter();
-  const {theme, setTheme} = useContext(ThemeContext);
-
+  // const [keyword, setKeyword] = useState("");
+  const [value, setValue] = useState("");
+  const [classValue, setClassValue]=useState("ride_children_button__ZA1aw")
+  const [option, setOption] = useState("");
   const getListData = async () => {
     console.log("router.query:", router.query);
     let page = +router.query.page || 1;
+
     if (page < 1) page = 1;
     try {
       const r = await fetch(RIDE_LIST + `?page=${page}`);
       const d = await r.json();
-
+      console.log(value)
       setData(d);
     } catch (ex) {}
   };
+  // const getListData = async () => {
+  //   // console.log("router.query:", router.query);
+  //   let page = +router.query.page || 1;
+  //   // 關鍵字搜尋
+  //   let keyword = router.query.keyword || ''
+  //   if (page < 1) page = 1;
+  //   try {
+  //     const r = await fetch(RIDE_LIST + `?${usp.toString()}`);
+  //     const d = await r.json();
+  //     // console.log(value)
+  //     setData(d);
+  //   } catch (ex) {
+  //     console.log(ex)
+  //   }
+  // };
 
+//   const getListData = async () => {
+//     const usp = new URLSearchParams(router.query)
+
+//     //console.log('router.query:', router.query)
+//     let page = +router.query.page || 1
+
+//     // 關鍵字搜尋
+//     let keyword = router.query.keyword || ''
+
+
+
+//     if (page < 1) page = 1
+//     try {
+//       const r = await fetch(BLOG_LIST + `?${usp.toString()}`)
+//       const d = await r.json()
+//       console.log(d)
+//       setData(d)
+//     } catch (ex) {
+//       console.log(ex)
+//     }
+//   }
+
+// return (
+//     <>
+// <input
+//                       type="search"
+//                       className={styles['bg-search']}
+//                       id="bg-search"
+//                       placeholder="請輸入搜尋關鍵字"
+//                       name="keyword"
+//                       onChange={(e) => {
+//                         // setKeyword(e.currentTarget.value)
+
+//                         router.push(
+//                           {
+//                             pathname: '/blog',
+//                             query: { ...router.query, keyword: e.target.value },
+//                           },
+//                           undefined,
+//                           { scroll: false }
+//                         )
+//                       }}
+//                     />
+
+// <>
+// }
   useEffect(() => {
     getListData();
   }, [router.query.page]);
@@ -39,36 +102,176 @@ export default function Ride() {
         <div className={styles.flex_center} style={{height:50}}>
           <span className={styles.flex_center}>
             <FaMagnifyingGlass style={{width:30,height:30.66,padding:5,borderRight:'none', position:'absolute',left:8}}/>
-            <input name='ride_name' className={styles.searchbar} type={'text'} placeholder={'請輸入設施名稱'}/>
+            <input name='ride_name' className={styles.searchbar} type={'text'} placeholder={'請輸入設施名稱'} 
+            value={value} 
+            onChange={(e) => {setValue(e.target.value);console.log(e.target.value)}}
+            // onChange={(e) => {setKeyword(e.currentTarget.value)
+            //   router.push(
+            //               {
+            //                 pathname: '/ride',
+            //                 query: { ...router.query, keyword: e.target.value },
+            //               },
+            //               undefined,
+            //               { scroll: false }
+            //             )
+            //           }}
+
+
+              />
           </span>
         </div>
         <div className={styles.flex_center_column}>
           <div className={styles.search_flex} style={{height:50}}>
             <span  className={styles.search_flex} style={{width:500}}>
               <span className={styles.button} style={{width:50}}>主題</span>
-              <span className={styles.children_button} style={{width:70}}>水世界</span>
-              <span className={styles.children_button} style={{width:85}}>冒險之旅</span>
-              <span className={styles.children_button} style={{width:85}}>慢樂悠遊</span>
+              <span className={classValue} id='theme_1' style={{width:70}} value={option} 
+              onClick={() => {
+                const elements=document.getElementsByClassName("ride_selected_children_button__GjYPq");
+                if(option==="")
+                {
+                  setOption("theme_1");
+                  console.log(option)}
+                else{
+                  setOption("");
+                  console.log(option);
+                };
+                if(classValue === 'ride_children_button__ZA1aw'){
+                  elements.classValue="ride_children_button__ZA1aw";
+                  setClassValue("ride_selected_children_button__GjYPq");
+                  console.log(classValue);
+                  }else{
+                    setClassValue("ride_children_button__ZA1aw");
+                    console.log(classValue)
+                  }
+                }}>水世界</span>
+              <span className={classValue} id='theme_2' style={{width:85}} value={option} onClick={() => {
+                if(option==="")
+                {setOption('theme_2');
+                console.log(option)}
+                else{
+                  setOption("");
+                  console.log(option);
+                };
+                const elements=document.getElementsByClassName(styles.selected_children_button);
+                if(classValue === 'ride_children_button__ZA1aw'){
+                  elements.classValue="ride_children_button__ZA1aw";
+                  setClassValue("ride_selected_children_button__GjYPq");
+                  console.log(classValue);
+                  }else{
+                    setClassValue("ride_children_button__ZA1aw");
+                    console.log(classValue)
+                  }
+                }}>冒險之旅</span>
+              <span className={styles.children_button} style={{width:85}} onClick={() => {
+                if(option==="")
+                {setOption('theme_3');
+                console.log(option)}
+                else{
+                  setOption("");
+                  console.log(option);
+                };
+                }}>慢樂悠遊</span>
+                <span className={styles.children_button} style={{width:85}} onClick={() => {
+                if(option==="")
+                {setOption('theme_4');
+                console.log(option)}
+                else{
+                  setOption("");
+                  console.log(option);
+                };
+                }}>樂高天堂</span>
             </span>
             <span  className={styles.search_flex} style={{width:500}}>
               <span className={styles.button} style={{width:50}}>類型</span>
-              <span className={styles.children_button} style={{width:85}}>兒童友善</span>
-              <span className={styles.children_button} style={{width:85}}>親子同樂</span>
-              <span className={styles.children_button} style={{width:85}}>刺激冒險</span>
+              <span className={styles.children_button} style={{width:85}} onClick={() => {
+                if(option==="")
+                {setOption('category_1');
+                console.log(option)}
+                else{
+                  setOption("");
+                  console.log(option);
+                };
+                }}>兒童友善</span>
+              <span className={styles.children_button} style={{width:85}} onClick={() => {
+                if(option==="")
+                {setOption('category_2');
+                console.log(option)}
+                else{
+                  setOption("");
+                  console.log(option);
+                };
+                }}>親子同樂</span>
+              <span className={styles.children_button} style={{width:85}} onClick={() => {
+                if(option==="")
+                {setOption('category_3');
+                console.log(option)}
+                else{
+                  setOption("");
+                  console.log(option);
+                };
+                }}>刺激冒險</span>
             </span>
           </div>
           <div className={styles.search_flex} style={{height:50}}>
             <span  className={styles.search_flex} style={{width:500}}>
               <span className={styles.button} style={{width:85}}>刺激程度</span>
-              <span className={styles.children_button} style={{width:35}}><FaStar /></span>
-              <span className={styles.children_button} style={{width:50}}><FaStar /><FaStar /></span>
-              <span className={styles.children_button} style={{width:65}}><FaStar /><FaStar /><FaStar /></span>
-              <span className={styles.children_button} style={{width:80}}><FaStar /><FaStar /><FaStar /><FaStar /></span>
-              <span className={styles.children_button} style={{width:95}}><FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></span>
+              <span className={styles.children_button} style={{width:35}} onClick={() => {
+                if(option==="")
+                {setOption('thillerRating_1');
+                console.log(option)}
+                else{
+                  setOption("");
+                  console.log(option);
+                };
+                }}><FaStar /></span>
+              <span className={styles.children_button} style={{width:50}} onClick={() => {
+                if(option==="")
+                {setOption('thillerRating_2');
+                console.log(option)}
+                else{
+                  setOption("");
+                  console.log(option);
+                };
+                }}><FaStar /><FaStar /></span>
+              <span className={styles.children_button} style={{width:65}} onClick={() => {
+                if(option==="")
+                {setOption('thillerRating_3');
+                console.log(option)}
+                else{
+                  setOption("");
+                  console.log(option);
+                };
+                }}><FaStar /><FaStar /><FaStar /></span>
+              <span className={styles.children_button} style={{width:80}} onClick={() => {
+                if(option==="")
+                {setOption('thillerRating_4');
+                console.log(option)}
+                else{
+                  setOption("");
+                  console.log(option);
+                };
+                }}><FaStar /><FaStar /><FaStar /><FaStar /></span>
+              <span className={styles.children_button} style={{width:95}} onClick={() => {
+                if(option==="")
+                {setOption('thillerRating_5');
+                console.log(option)}
+                else{
+                  setOption("");
+                  console.log(option);
+                };
+                }}><FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></span>
             </span>
             <span  className={styles.search_flex} style={{width:500}}>
               <span className={styles.button} style={{width:85}}>特殊支援</span>
-              <span className={styles.children_button} style={{width:100}}>輪椅可搭乘</span>
+              <span className={styles.children_button} style={{width:100}} onClick={() => {
+                if(option==="")
+                {setOption('support_2');
+                console.log(option)}
+                else{
+                  setOption("");
+                  console.log(option);
+                };
+                }}>輪椅可搭乘</span>
             </span>
           </div>
           <div className={styles.card_flex}>
