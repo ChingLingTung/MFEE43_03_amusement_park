a093f20d530adb2307f8e07219f2cea6b0b38b3a
ChingLingTung <littlesmall0540@gmail.com>
2024-01-19 16:52:46 +0800

    處理merge問題

--- next/pages/user/index.js
+++ next/pages/user/index.js
@@ -39,7 +39,7 @@ export default function UserInfo() {
   },[parkAuth])
 
   useEffect(()=>{
-    if(!parkAuth.email){
+    if(!parkAuth.email && !data){
       Alert.fire({ 
   didOpen: () => { 
       Alert.fire({
@@ -56,7 +56,7 @@ export default function UserInfo() {
     }
 })
 }
-  },[parkAuth.email])
+  },[parkAuth.id])
 
   return (
     <>
