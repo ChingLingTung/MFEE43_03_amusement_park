fc4fe5db4beeab726843bac22985d97e492550e9
APPLE\USER <justin870605@gmail.com>
2024-01-25 16:15:35 +0800

    更新票券購物車

--- next/component/Userpay/Paystep/Paystep.js
+++ next/component/Userpay/Paystep/Paystep.js
@@ -4,12 +4,15 @@ import Image from "next/image";
 import { FaChevronRight } from "react-icons/fa";
 import { FaChevronLeft } from "react-icons/fa";
 import styles from "@/component/Userpay/Paystep/Paystep.module.css";
+import Link from "next/link";
 
 export default function Paystep() {
   return (
     <>
       <div className={styles.shopping_step}>
-        <div>1. 您的購物車</div>
+        <Link href={"/cart/ticketCart"}>
+          <div>1. 您的購物車</div>
+        </Link>
         <div>
           <FaChevronRight />
         </div>
